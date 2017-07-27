3fd81b5631463bec84beace7256cad02291884e1
Grant Broadwater <grant.broadwater@garmin.com>
2017-07-27 14:33:01 -0500

    Add firebase

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
+var ref = firebase.database().ref();
 
+$( document ).ready( function() {
+
+  var authorRef = ref.child( 'author' );
+
+  authorRef.once( 'value', function( snapshot ) {
+
+    alert( "Author: " + snapshot.val() );
+
+  } );
+
+} );
